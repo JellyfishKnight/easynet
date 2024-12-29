d5724612f5e4b529627ceef4e047f1f797a71ee3
JellyfishKnight <knightjellyfish@gmail.com>
2024-12-29 21:39:06 +0800

    fixed Cmake of cpp 20

--- utils/include/print.hpp
+++ utils/include/print.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <iostream>
+#include <concepts>
+#include <ranges>
+
 
 namespace utils {
 
+template<typename BasicType>
+void print(BasicType value) {
+    std::cout << value << std::endl;
+}
 
+// print container type
+template<typename ContainerType>
+    requires std::ranges::range<ContainerType>
+void print(const ContainerType& container) {
+    for (const auto& element : container) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
 
 
 
