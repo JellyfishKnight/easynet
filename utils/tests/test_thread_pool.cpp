0a4b024a47934428a7781ca74aee55deb400c7c0
JellyfishKnight <knightjellyfish@gmail.com>
2025-01-14 18:58:29 +0800

    all test passed

--- utils/tests/test_thread_pool.cpp
+++ utils/tests/test_thread_pool.cpp
@@ -22,13 +22,13 @@ protected:
 };
 
 TEST_F(ThreadPoolTest, TestThreadPool) {
-    std::vector<std::future<int>> futures(1);
-    for (int i = 0; i < 1; ++i) {
+    std::vector<std::future<int>> futures;
+    for (int i = 0; i < 10; ++i) {
         auto f = [](int a, int b) { return a + b; };
         auto opt = pool->submit(std::to_string(i), f, i, i + 1);
         if (opt.has_value()) {
             try {
-                futures[i] = std::move(opt.value());
+                futures.emplace_back(std::move(opt.value()));
             } catch (const std::future_error& e) {
                 FAIL() << e.what();
             }
@@ -36,10 +36,9 @@ TEST_F(ThreadPoolTest, TestThreadPool) {
             FAIL() << "Failed to submit task";
         }
     }
-    for (int i = 0; i < 1; ++i) {
+    for (int i = 0; i < 10; ++i) {
         try {
             ASSERT_EQ(futures[i].get(), i + i + 1);
-            std::cout << futures[i].get() << std::endl;
         } catch (const std::future_error& e) {
             FAIL() << e.what();
         }
@@ -64,7 +63,7 @@ TEST_F(ThreadPoolTest, AddTaskWithName) {
 
 TEST_F(ThreadPoolTest, GetStatusOfTask) {
     auto f = [](int a, int b) {
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+        std::this_thread::sleep_for(std::chrono::microseconds(100));
         return a + b;
     };
     std::vector<std::future<int>> futures;
