d1615f64c0f8e8ba06144c1bf8624fa6603c40a2
JellyfishKnight <knightjellyfish@gmail.com>
2025-01-18 18:33:12 +0800

    add epoll, add connection

--- net/common/include/connection.hpp
+++ net/common/include/connection.hpp
@@ -4,41 +4,53 @@
 #include "thread_pool.hpp"
 #include <cassert>
 #include <cstddef>
+#include <format>
 #include <functional>
 #include <memory>
 #include <string>
 #include <sys/socket.h>
+#include <system_error>
 #include <thread>
 #include <unordered_map>
 
 namespace net {
 
+enum class ConnectionStatus { CONNECTED, DISCONNECTED };
+
+template<typename ResType, typename ReqType>
+struct Connection: std::enable_shared_from_this<Connection<ResType, ReqType>> {
+    int m_fd;
+    addressResolver::address m_addr;
+    std::function<ResType(const ReqType&)> m_handler;
+    ConnectionStatus m_status = ConnectionStatus::DISCONNECTED;
+
+    virtual std::function<ResType(const ReqType&)> get_handler() {
+        return m_handler;
+    }
+};
+
 template<typename ResType, typename ReqType>
 class Server: std::enable_shared_from_this<Server<ResType, ReqType>> {
 public:
-    enum class ConnectionStatus { CONNECTED, DISCONNECTED };
-
-    struct Connection {
-        int m_fd;
-        addressResolver::address m_addr;
-        std::function<ResType(const ReqType&)> m_handler;
-        ConnectionStatus m_status = ConnectionStatus::DISCONNECTED;
-    };
-
     /**
      * @brief Construct a new Server object
      * @param ip ip address to bind to
      * @param service service to bind to
-     * @throw std::system_error if failed to create socket, bind or listen
      */
-    Server(const std::string& ip, const std::string& service): m_ip(ip), m_service(service) {
+    Server(const std::string& ip, const std::string& service): m_ip(ip), m_service(service) {}
+
+    /**
+     * @brief Listen on the socket
+     * @throw std::system_error if failed to listen on socket
+     */
+    void listen() {
         m_lisen_fd = ::socket(AF_INET, SOCK_STREAM, 0);
         if (m_lisen_fd == -1) {
             throw std::system_error(errno, std::system_category(), "Failed to create socket");
         }
 
         addressResolver resolver;
-        auto addr_info = resolver.resolve(ip, service);
+        auto addr_info = resolver.resolve(m_ip, m_service);
         if (::bind(m_lisen_fd, addr_info.get_address().m_addr, addr_info.get_address().m_len) == -1)
         {
             throw std::system_error(errno, std::system_category(), "Failed to bind socket");
@@ -49,6 +61,9 @@ public:
         }
     }
 
+    /*
+     * @brief Start the server
+    */
     void start() {
         m_stop = false;
         m_accept_thread = std::thread([this]() {
@@ -57,11 +72,11 @@ public:
                 socklen_t len;
                 int client_fd = ::accept(m_lisen_fd, &client_addr.m_addr, &len);
                 if (client_fd == -1) {
-                    throw std::system_error(
-                        errno,
-                        std::system_category(),
-                        "Failed to accept connection"
-                    );
+                    std::cerr << std::format(
+                        "Failed to accept connection: {}",
+                        std::error_code(errno, std::system_category()).message()
+                    ) << std::endl;
+                    continue;
                 }
                 if (m_connections.contains(m_ip)) {
                     if (m_connections[m_ip].contains(m_service)) {
@@ -115,10 +130,17 @@ public:
         ::close(m_lisen_fd);
     }
 
+    /**
+     * @brief Enable thread pool
+     * @param num_threads number of threads in the pool
+     */
     void enable_thread_pool(std::size_t num_threads) {
         m_thread_pool = std::make_shared<utils::ThreadPool>(num_threads);
     }
 
+    /**
+     * @brief Disable thread pool
+     */
     void disable_thread_pool() {
         m_thread_pool->stop();
         while (m_thread_pool->is_running())
@@ -126,6 +148,22 @@ public:
         m_thread_pool.reset();
     }
 
+    /**
+     * @brief Set the thread pool size
+     * @param num_threads number of threads in the pool
+     */
+    void set_thread_pool_size(std::size_t num_threads) {
+        if (m_thread_pool) {
+            if (m_thread_pool->worker_num() == num_threads) {
+                return;
+            }
+            m_thread_pool->resize_worker(num_threads);
+        }
+    }
+
+    /**
+     * @brief Stop the server
+     */
     void stop() {
         m_stop = true;
         m_accept_thread.join();
@@ -143,26 +181,28 @@ public:
         }
     }
 
-    // /**
-    //  * @brief Start the server
-    //  * @throw std::system_error if failed to create epoll
-    //  */
-    // void enable_epoll() {
-    //     m_epoll_fd = ::epoll_create1(0);
-    //     if (m_epoll_fd == -1) {
-    //         throw std::system_error(errno, std::system_category(), "Failed to create epoll");
-    //     }
-    //     struct epoll_event event;
-    //     event.events = EPOLLIN;
-    //     event.data.fd = m_lisen_fd;
-    //     if (::epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, m_lisen_fd, &event) == -1) {
-    //         throw std::system_error(
-    //             errno,
-    //             std::system_category(),
-    //             "Failed to add listen socket to epoll"
-    //         );
-    //     }
-    // }
+    void enable_epoll() {
+        // set none blocking
+        if (::fcntl(m_lisen_fd, F_GETFL, 0) == -1 || ::fcntl(m_lisen_fd, F_SETFL, O_NONBLOCK) == -1)
+        {
+            throw std::system_error(errno, std::system_category(), "Failed to get socket flags");
+        }
+
+        m_epoll_fd = ::epoll_create1(0);
+        if (m_epoll_fd == -1) {
+            throw std::system_error(errno, std::system_category(), "Failed to create epoll");
+        }
+        struct epoll_event event;
+        event.events = EPOLLIN;
+        event.data.fd = m_lisen_fd;
+        if (::epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, m_lisen_fd, &event) == -1) {
+            throw std::system_error(
+                errno,
+                std::system_category(),
+                "Failed to add listen socket to epoll"
+            );
+        }
+    }
 
 private:
     std::string m_ip;
@@ -177,7 +217,8 @@ private:
     utils::ThreadPool::SharedPtr m_thread_pool;
 
     // first key : ip, second key : port
-    std::unordered_map<std::string, std::unordered_map<std::string, Connection>> m_connections;
+    std::unordered_map<std::string, std::unordered_map<std::string, Connection<ReqType, ResType>>>
+        m_connections;
 };
 
 template<typename ResType, typename ReqType>
