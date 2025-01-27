e1a4121eb34b6f86ad811d8ae11f073a7c7ec25b
JellyfishKnight <knightjellyfish@gmail.com>
2025-01-27 23:55:27 +0800

    https client tested

--- demo/https_client.cpp
+++ demo/https_client.cpp
@@ -2,5 +2,38 @@
 #include "ssl.hpp"
 
 int main() {
+    net::HttpClient client("127.0.0.1", "8080");
+
+    auto ctx = net::SSLContext::create();
+    ctx->set_certificates(
+        "/home/jk/Projects/net/keys/certificate.crt",
+        "/home/jk/Projects/net/keys/private.key"
+    );
+
+    client.add_ssl_context(ctx);
+
+    client.connect_server();
+
+    while (true) {
+        std::string input;
+        std::cin >> input;
+        if (input == "exit") {
+            client.close();
+            return 0;
+        }
+        if (input == "s") {
+            input.clear();
+        }
+        auto res = client.get("/" + input);
+        std::cout << "Http version: " << res.version << std::endl;
+        std::cout << "Status Code: " << static_cast<int>(res.status_code) << std::endl;
+        std::cout << "Reason: " << res.reason << std::endl;
+        std::cout << "Headers: " << std::endl;
+        for (auto& [key, value]: res.headers) {
+            std::cout << key << ": " << value << std::endl;
+        }
+        std::cout << "Body: " << res.body << std::endl;
+    }
+
     return 0;
 }
