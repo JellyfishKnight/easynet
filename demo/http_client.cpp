5fc2990d655354e7d224b1313e369f94ddd40c91
JellyfishKnight <knightjellyfish@gmail.com>
2025-02-20 16:45:40 +0800

    fixed bug in http server

--- demo/http_client.cpp
+++ demo/http_client.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 
 int main() {
-    net::HttpClient client("www.baidu.com", "80");
+    net::HttpClient client("127.0.0.1", "8080");
     client.set_proxy("127.0.0.1", "2196");
 
     auto err = client.connect_server();
